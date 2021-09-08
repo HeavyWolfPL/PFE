e39e3583f643fa94caf60ebceffcb7a4cb5241f1
Wafelowski <heavywolfpl@gmail.com>
2021-09-08 17:00:22 +0200

    version bump once again

--- PFE/EFE.cs
+++ PFE/EFE.cs
@@ -9,7 +9,7 @@ namespace EFE
 		public override string Author { get; } = "Wafel & Cyanox";
 		public override string Name { get; } = "EveryoneFckingExplodes";
 		public override string Prefix { get; } = "EFE";
-		public override Version Version { get; } = new Version(3, 0, 2);
+		public override Version Version { get; } = new Version(3, 1, 0);
 		public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
 
 
